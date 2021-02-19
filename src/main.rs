f26f0f3dfb18450d3d1ed5907ba21acae0f8ff13
Michael <michaelpurtill123@hotmail.com>
2021-02-19 08:11:39 -0500

    i don't need the ops enum

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,6 @@ fn main() {
 
 }
 
-enum Op {
-    plus,
-    minus,
-    mult,
-    div,
-}
-
 enum NodeVal {
     Int(String),
     N(Box<Node>),
@@ -16,7 +9,7 @@ enum NodeVal {
 }
 
 struct Node {
-    operator: Op,
+    operator: String,
     left: NodeVal,
     right: NodeVal,
 }
@@ -32,9 +25,26 @@ fn math(exp: String) -> i32 {
             return tokens[0].parse::<i32>().unwrap();
         }
 
-        let mut masterNode = Node {operator: Op::plus, left: NodeVal:: Undefined, right: NodeVal:: Undefined};
+        let mut masterNode = Node {operator:"".to_string(), left: NodeVal:: Undefined, right: NodeVal:: Undefined};
+
+        if tokens.len() == 3 {
+            masterNode.operator = tokens[1].to_string();
+            masterNode.left = NodeVal::Int(tokens[0].to_string());
+            masterNode.right = NodeVal::Int(tokens[2].to_string());
+
+            return evalNode(masterNode);
+        }
+        else {
+            for i in 0..tokens.len() {
+
+            }
+        }
 
         1
 
 
+}
+
+fn evalNode(node: Node) -> i32 {
+    return 1;
 }
